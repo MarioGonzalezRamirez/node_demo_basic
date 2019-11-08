7db21cd30b26cca3bb2938f2ca0c452f928d54aa
Period 5 <mariogonzalezramirez@students.berkeley.net>
2019-11-08 14:01:36 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express    = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+var app = express()
+
+mongoose.connect("mongodb+srv://malbinson:malbinson1@cluster0-cvp0r.mongodb.net/test?retryWrites=true&w=majority")
+
+//set up vars
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//link to data model and routes
+var Cat = require('./cat')
+require('./routes')(app);
+
+app.listen(3000, () => {
+    console.log('meow')
+})
